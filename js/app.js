c2cc70081b2c064cace5e0e3e87d414d87e0540a
hafijure <hafijure@gmail.com>
2023-05-04 01:57:34 +0600

    pin match function working fine

--- js/app.js
+++ js/app.js
@@ -19,3 +19,43 @@ document.getElementById('btn-generator').addEventListener('click',function(){
     pinDisplayElement.value = pin;
 })
 
+document.getElementById('calculator').addEventListener('click',function(event){
+    const typedPin = event.target.innerText;
+    const typednumberfield = document.getElementById('number-field');
+    const previousTypedPin = typednumberfield.value;
+    if (isNaN(typedPin)) {
+        if (typedPin === 'C') {
+            typednumberfield.value = '';
+        }
+        else if(typedPin === '<'){
+            const digits = previousTypedPin.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typednumberfield.value = remainingDigits;
+
+        }
+    }else{
+        const newTypedNumebr = previousTypedPin + typedPin;
+        typednumberfield.value = newTypedNumebr;
+    }
+
+})
+
+document.getElementById('btn-submit').addEventListener('click',function(){
+    const pinDisplayElement = document.getElementById('pin-field');
+    const generatedPin = pinDisplayElement.value;
+
+    const typednumberfield = document.getElementById('number-field');
+    const typedPin = typednumberfield.value;
+    const getSuccessSms = document.getElementById('success-message');
+    const failSms = document.getElementById('failed-message');
+    if (generatedPin === typedPin) {
+        
+        getSuccessSms.style.display = 'block';
+        failSms.style.display = 'none';
+    }else{
+        
+        failSms.style.display = 'block';
+        getSuccessSms.style.display = 'none';
+    }
+})
